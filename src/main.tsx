85c35c50b8ac2176622ebe5bc1b1f99262a90561
YernarT <toktaryernar@gmail.com>
2022-11-14 23:21:52 +0600

    update: :hammer: add react-query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Providers
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { SnackbarProvider } from 'notistack';
+// App
 import App from './App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<SnackbarProvider>
-			<App />
-		</SnackbarProvider>
+		<QueryClientProvider client={queryClient}>
+			<SnackbarProvider>
+				<App />
+			</SnackbarProvider>
+		</QueryClientProvider>
 	</React.StrictMode>,
 	document.getElementById('root') as HTMLElement,
 );
