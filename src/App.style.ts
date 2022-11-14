ffecbe16c802ba0f3708a140e6d06a479b9e458a
YernarT <toktaryernar@gmail.com>
2022-11-14 16:37:34 +0600

    feat: :sparkles: App style

---
+++ src/App.style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const AppContainer = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background: #667db6;
+	background: linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6);
+
+	overflow: auto hidden;
+	position: relative;
+`;
