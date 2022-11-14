1453f7a907047e3056a9a822bc51407d2d85ab2b
YernarT <toktaryernar@gmail.com>
2022-11-14 23:28:26 +0600

    update: :hammer: add handle delete

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -17,16 +17,23 @@ import { TodoListStyled } from './style';
 
 interface TodoListProps {
 	todoList: I_Todo[];
+	handleDeleteTodo: Function;
 }
 
-export default memo(function TodoList({ todoList }: TodoListProps) {
+export default memo(function TodoList({
+	todoList,
+	handleDeleteTodo,
+}: TodoListProps) {
 	return (
 		<TodoListStyled>
 			{todoList.map(todo => (
 				<ListItem
 					key={todo.id}
 					secondaryAction={
-						<IconButton edge="end" aria-label="delete">
+						<IconButton
+							edge="end"
+							aria-label="delete"
+							onClick={() => handleDeleteTodo(todo.id)}>
 							<DeleteIcon color="error" />
 						</IconButton>
 					}
